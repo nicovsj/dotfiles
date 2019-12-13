5f4838c7800214f1bfcda66d2f26dfa1d4773bd0
Nicolás Van Sint Jan <nicovsj@uc.cl>
2019-12-12 23:12:57 -0300

    Fixed polybar launcher for multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,8 +6,15 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+		MONITOR=$m polybar --reload bottom &
+  done
+else
+  polybar --reload top &
+  polybar --reload bottom &
+fi
 # Launch bar1 and bar2
-polybar top &
-polybar bottom &
 
 echo "Bars launched..."
